1a6ac84c8d6cc414464776e0b22c69a26116d23b
Amina <aminabegum0065@gmail.com>
2026-08-28 23:53:25 +0600

    Update server.js

--- moodtracker/web app/backend/src/server.js
+++ moodtracker/web app/backend/src/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // CORS Configuration
 const corsOptions = {
-  origin: 'http://localhost:3000', // Frontend URL
+  origin: '*', // Frontend URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
